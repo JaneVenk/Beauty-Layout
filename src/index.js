3b38633e45c5676695097e878c2d1611bcf45214
Jane Venkova <jenn86@mail.ru>
2023-02-27 11:05:48 +0400

    Add files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./sass/main.scss";
+
+import logoImage from "./images/logo.svg";
+import chair from "./images/chair.svg";
+import envelope from "./images/envelope.svg";
+import youtube from "./images/youtube.svg";
+import vk from "./images/vk.svg";
+import facebook from "./images/facebook.svg";
+import instagram from "./images/instagram.svg";
+
+document.getElementById("logoImage").src = logoImage;
+document.getElementById("chair1").src = chair;
+document.getElementById("chair2").src = chair;
+document.getElementById("chair3").src = chair;
+document.getElementById("chair4").src = chair;
+document.getElementById("envelope").src = envelope;
+document.getElementById("youtube").src = youtube;
+document.getElementById("vk").src = vk;
+document.getElementById("facebook").src = facebook;
+document.getElementById("instagram").src = instagram;
+
+
+
+
+const actions = require("./js/actions");
